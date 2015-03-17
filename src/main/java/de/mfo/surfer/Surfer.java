b1d40d3f46b6fb5a52db0c1b29bb61b56393ab24
porst17 <stussak@mfo.de>
2015-03-17 11:26:19 +0100

    custom color picker implementation

--- src/main/java/de/mfo/surfer/Surfer.java
+++ src/main/java/de/mfo/surfer/Surfer.java
@@ -2,11 +2,14 @@ package de.mfo.surfer;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import jfxtras.labs.scene.layout.ScalableContentPane;
+
+import de.mfo.surfer.ui.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +26,23 @@ public class Surfer extends Application
         @Override
         public void start( Stage stage )
                 throws Exception
-	{
+	    {
                 logger.info( "loading user interface" );
 
+                FXMLLoader fxmlLoader = new FXMLLoader( getClass().getResource( "Surfer.fxml" ) );
+
                 ScalableContentPane scp = new ScalableContentPane();
-                scp.getContentPane().getChildren().add( FXMLLoader.load( getClass().getResource( "Surfer.fxml" ) ) );
+                scp.getContentPane().getChildren().add( fxmlLoader.load() );
 
                 Scene scene = new Scene( scp );
+                scene.getStylesheets().add(getClass().getResource("css/color.css").toExternalForm());
 
-                stage.setTitle( "SURFER" );
+                VBox colorPickersVBox = ( VBox ) scp.lookup( "#colorPickers" );
+                colorPickersVBox.getChildren().clear();
+                colorPickersVBox.getChildren().add( new CustomColorPicker() );
+                colorPickersVBox.getChildren().add( new CustomColorPicker() );
+
+                stage.setTitle("SURFER");
                 stage.setScene( scene );
                 stage.setWidth( 1280 );
                 stage.setHeight( 720 );
